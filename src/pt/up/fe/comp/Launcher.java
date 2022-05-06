4c20c6abfd87f4f0db21cf060c6c830984bea046
Vasco52 <up201808031@g.uporto.pt>
2022-05-06 18:02:06 +0100

    do more stuff

--- src/pt/up/fe/comp/Launcher.java
+++ src/pt/up/fe/comp/Launcher.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp.jmm.parser.JmmParserResult;
 import pt.up.fe.specs.util.SpecsIo;
 import pt.up.fe.specs.util.SpecsLogs;
@@ -52,6 +53,15 @@ public class Launcher {
 
         // Check if there are parsing errors
         TestUtils.noErrors(analysisResult.getReports());
+
+        // Instantiate JmmOptimization
+        JmmOptimizer optimizer = new JmmOptimizer();
+
+        // Optimization stage
+        OllirResult ollirResult = optimizer.toOllir(analysisResult);
+
+        // Check if there are parsing errors
+        TestUtils.noErrors(ollirResult.getReports());
 
         // ... add remaining stages
     }
